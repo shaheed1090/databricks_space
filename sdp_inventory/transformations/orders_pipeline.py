a0b0635c1e208ec9eab2c05211247b098afe692e
shaheed vk <shaheed1090@gmail.com>
2025-11-16 23:14:39 +0100

    Delete sdp_inventory directory

--- sdp_inventory/transformations/orders_pipeline.py
+++
@@ -1,9 +0,0 @@
-from pyspark import pipelines as dp
-
-@dp.table(name="inventory.orders.bronze_order")
-def bronze_order():
-    return (
-        spark.readStream.format("cloudFiles")
-        .option("cloudFiles.format", "json")
-        .load("/Volumes/inventory/source_data/orders/orders_000.json")
-    )
